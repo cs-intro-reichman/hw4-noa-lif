128d1afc49b1d76bc06151f4672dd944b3ee192f
noa-lif <noalif65@gmail.com>
2024-12-03 15:46:30 +0200

    fix input in Primes

--- Primes.java
+++ Primes.java
@@ -2,8 +2,7 @@
 public class Primes {
 
     public static void main(String[] args) {
-        // int n = Integer.parseInt(args[0]);
-        int n=7;
+        int n = Integer.parseInt(args[0]);
         boolean[] boolArr = new boolean[n + 1];
 
         for (int i = 2; i < n+1; i++) {
